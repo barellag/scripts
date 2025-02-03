5c09122ebd3802c13846fd62c11aad50e28092eb
Gabriel Barella <gabriel.barella@icoud.com>
2025-02-03 18:39:42 -0300

    optimizations on auth

--- GCP_get_sync_id_test.py
+++ GCP_get_sync_id_test.py
@@ -9,36 +9,40 @@ appliance_ip = input("Enter your appliance IP or hostname: ")
 
 request_url = "https://"+appliance_ip+"/api/"
 private_token_url = request_url+"oauth2/token"
-username = input("Enter the username: ")
-password = getpass.getpass("Enter Password: ")
 
 
-payload = {
-  "grant_type": "password",
-  "username": username,
-  "password": password,
-  "use_short_term_refresh": "false",
-  "rememberMe": "true"
-}
+# Initialize a variable to control the loop
+authenticated = False
 
-headers = {
-  "x-api-version": "1.0-rev4",
-}
+# Start the loop for user authentication
+while not authenticated:
+    username = input("Enter the username: ")
+    password = getpass.getpass("Enter Password: ")
 
-response = requests.post(private_token_url, data=payload, headers=headers, verify=False)
+    payload = {
+        "grant_type": "password",
+        "username": username,
+        "password": password,
+        "use_short_term_refresh": "false",
+        "rememberMe": "true"
+    }
 
-# Extracting the token from response
-if response.status_code == 200:
-    print("Token retrieved successfully!")
-    token_info = response.json()  # Parse JSON response
-    #print(token_info)  # This will print the entire JSON response including the token
-    token = token_info.get('access_token')  # Accessing the token directly
-    #print("Access Token:", token)
-else:
-    print("Failed to retrieve token")
-    print("Status Code:", response.status_code)
-    print("Response:", response.text)
+    headers = {
+        "x-api-version": "1.0-rev4",
+    }
 
+    response = requests.post(private_token_url, data=payload, headers=headers, verify=False)
+
+    # Check response status to determine if authentication is successful
+    if response.status_code == 200:
+        print("Token retrieved successfully!")
+        token_info = response.json()  # Parse JSON response
+        token = token_info.get('access_token')  # Retrieve the token
+        authenticated = True  # Set the flag to exit the loop
+    else:
+        print("Failed to retrieve token. Please check your credentials and try again.")
+        print("Status Code:", response.status_code)
+        print("Response:", response.text)
 
 #Calling Synchronization ID
 
--- GCP_private_token.py
+++ GCP_private_token.py
@@ -1,5 +1,7 @@
 import requests
 import getpass
+import json
+
 
 #authentication for private endpoints
 
@@ -7,33 +9,48 @@ appliance_ip = input("Enter your appliance IP or hostname: ")
 
 request_url = "https://"+appliance_ip+"/api/"
 private_token_url = request_url+"oauth2/token"
-username = input("Enter the username: ")
-password = getpass.getpass("Enter Password: ")
 
 
-payload = {
-  "grant_type": "password",
-  "username": username,
-  "password": password,
-  "use_short_term_refresh": "false",
-  "rememberMe": "true"
-}
+# Initialize a variable to control the loop
+authenticated = False
+
+# Start the loop for user authentication
+while not authenticated:
+    username = input("Enter the username: ")
+    password = getpass.getpass("Enter Password: ")
+
+    payload = {
+        "grant_type": "password",
+        "username": username,
+        "password": password,
+        "use_short_term_refresh": "false",
+        "rememberMe": "true"
+    }
+
+    headers = {
+        "x-api-version": "1.0-rev4",
+    }
+
+    response = requests.post(private_token_url, data=payload, headers=headers, verify=False)
+
+    # Check response status to determine if authentication is successful
+    if response.status_code == 200:
+        print("Token retrieved successfully!")
+        token_info = response.json()  # Parse JSON response
+        token = token_info.get('access_token')  # Retrieve the token
+        authenticated = True  # Set the flag to exit the loop
+    else:
+        print("Failed to retrieve token. Please check your credentials and try again.")
+        print("Status Code:", response.status_code)
+        print("Response:", response.text)
+
+#Calling Synchronization ID
+
+endpoint_url = request_url+"v1/collection/synchronizationItems?fromUsn=0&creationTimeUtc=2024-04-24T19%3A19%3A04&pageSize=2048&includeTombstones=true"
 
 headers = {
-  "x-api-version": "1.0-rev4",
+    "x-api-version": "1.0-rev4",
+    "Authorization": "bearer "+token
 }
-
-response = requests.post(private_token_url, data=payload, headers=headers, verify=False)
-
-# Extracting the token from response
-if response.status_code == 200:
-    print("Token retrieved successfully!")
-    token_info = response.json()  # Parse JSON response
-    #print(token_info)  # This will print the entire JSON response including the token
-    token = token_info.get('access_token')  # Accessing the token directly
-    print("Access Token:", token)
-else:
-    print("Failed to retrieve token")
-    print("Status Code:", response.status_code)
-    print("Response:", response.text)
+response = requests.get(endpoint_url, headers=headers, verify=False)
 
